4dbf3a63f1cdc840b8c05f9867f4a008d9fe168b
Dis <sr_5@inbox.ru>
2013-04-20 00:23:34 +0700

    Cart created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+    def current_cart
+        Cart.find(session[:cart_id])
+      rescue ActiveRecord::RecordNotFound
+        cart = Cart.create
+        session[:cart_id] = cart.id
+        cart
+    end
+
+    def increment_count
+      # if session[:counter].nil?
+      #   session[:counter] = 0
+      # end
+      ### equal to next one line
+      session[:counter] ||= 0
+      session[:counter] += 1
+    end
+
+    def reset_session_counter
+      # ...
+      session[:counter] = 0
+    end
 end
