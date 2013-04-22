a33f369e0bbbcd0d08733d712b32c205bdd7f53f
Dis <sr_5@inbox.ru>
2013-04-22 18:17:49 +0700

    Order capturing finished

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -34,8 +34,8 @@ class OrdersControllerTest < ActionController::TestCase
     assert_difference('Order.count') do
       post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
-
-    assert_redirected_to order_path(assigns(:order))
+    assert_redirected_to store_path
+    #assert_redirected_to order_path(assigns(:order))
   end
 
   test "should show order" do
