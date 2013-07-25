7bf9acd63aa4a7eea1602a5c5336a67e892f1a9e
Dis <sr_5@inbox.ru>
2013-07-25 21:49:58 +0700

    Translated select box on purchase form

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,12 @@
 module OrdersHelper
+
+  # This creates a hash from the PAYMENT_TYPES array. The same should work if you have stored the payment types in the database. This hash is then passed as an option to the options_for_select method.
+  def payment_type_options
+    options_for_select(Hash[ Order::PAYMENT_TYPES.map { |x| [ 
+        I18n.t('orders.payment_types.' + x.parameterize.underscore), x 
+        ] } 
+    ])    
+  end
+
 end
+  
